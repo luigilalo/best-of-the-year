11a86e7b943a34d02a49db4989e29eb948176e6f
luigilalo <luigilaloe1@gmail.com>
2024-01-04 14:06:58 +0100

    Initial Commit

---
+++ src/main/java/learning/org/bestoftheyear/BestoftheyearApplication.java
@@ -0,0 +1,13 @@
+package learning.org.bestoftheyear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestoftheyearApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestoftheyearApplication.class, args);
+	}
+
+}
