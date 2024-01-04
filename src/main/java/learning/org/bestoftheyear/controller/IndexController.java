bb2ff4263e20499150b6b6506cf6ac034e38d1a3
luigilalo <luigilaloe1@gmail.com>
2024-01-04 14:23:58 +0100

    Done Step 1

--- src/main/java/learning/org/bestoftheyear/controller/IndexController.java
+++ src/main/java/learning/org/bestoftheyear/controller/IndexController.java
@@ -1,4 +1,14 @@
 package learning.org.bestoftheyear.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+@RequestMapping("/")
 
 public class IndexController {
+    public String home (Model model) {
+        model.addAttribute("anno", 2023);
+        model.addAttribute("nome", "Luigi");
+        return "home";
+    }
 }
